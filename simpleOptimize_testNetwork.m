44ac30b200fe407105da7064be25c5e77322aeac
Benjamin Ostow <bostow26@cgps.org>
2025-03-14 09:35:18 -0400

    Update simpleOptimize_testNetwork.m

--- simpleOptimize_testNetwork.m
+++ simpleOptimize_testNetwork.m
@@ -1,3 +1,12 @@
+% Load the .nii file to get the data
+nii_data = niftiread('/Users/benjaminostow/Downloads/Language_Network_Atlas_1_9_25/cleanedoutput500.nii');
+
+% Create a mask for non-zero regions (your target area)
+target_mask = nii_data > 0;  % Change condition based on your target regions
+
+% Find the linear indexes of the target regions
+target_indexes = find(target_mask);
+
 % Initialize structure
 opt = opt_struct('TDCSoptimize');
 
@@ -9,13 +18,18 @@ opt.name = 'optimization/network_target';
 
 % Select a maximum total current (in A)
 opt.max_total_current = 2e-3;
+
 % Select a maximum current at each electrode (in A)
 opt.max_individual_current = 1e-3;
+
 % Select a maximum number of active electrodes (optional)
 opt.max_active_electrodes = 8;
 
-% Define the target using a .nii mask
-opt.target.nii_file = '/Users/benjaminostow/Downloads/Language_Network_Atlas_1_9_25/cleanedoutput500.nii.gz';
+% Define the target using the .nii file
+opt.target.nii_file = '/Users/benjaminostow/Downloads/Language_Network_Atlas_1_9_25/cleanedoutput500.nii';
+
+% Set the indexes of the target regions
+opt.target.indexes = target_indexes;  % Using the extracted target indexes
 
 % Define the desired intensity of the electric field (in V/m)
 opt.target.intensity = 0.3;  % Increase EF strength for optimization
