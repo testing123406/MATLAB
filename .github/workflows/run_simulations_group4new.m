8a965bd9a2c6d490cf387cb3d5207027cb0ad1e8
Benjamin Ostow <bostow26@cgps.org>
2025-04-02 13:27:27 -0400

    Update run_simulations_group4new.m

--- .github/workflows/run_simulations_group4new.m
+++ .github/workflows/run_simulations_group4new.m
@@ -1,5 +1,5 @@
 % Add SimNIBS to the MATLAB path
-addpath(''/Users/benjaminostow/Applications/SimNIBS-4.1/matlab_tools');
+addpath('/Users/benjaminostow/Applications/SimNIBS-4.1/matlab_tools');
 
 % Optional: Save the path for future sessions
 savepath;
